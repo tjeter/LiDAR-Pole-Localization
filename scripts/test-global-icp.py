547261908d3dd423d488a6bddee6a8015c57b285
Tre' Jeter <33637310+tjeter@users.noreply.github.com>
2024-09-25 14:14:56 -0700

    Update test-global-icp.py

--- scripts/test-global-icp.py
+++ scripts/test-global-icp.py
@@ -3,11 +3,11 @@ import numpy as np
 import pandas as pd
 
 # Load your global map
-global_map_file = 'globalmap_3.npz'  # Replace with the actual file path
+global_map_file = 'globalmap_3.npz'  
 
 # Load global map point cloud
 with np.load(global_map_file, allow_pickle=True) as data:
-    global_map_points = data['polemeans'][:, :2]  # Assuming 'polemeans' contains the XY coordinates of poles
+    global_map_points = data['polemeans'][:, :2]  
 
 # Add a third column for Z-coordinate (zeros in this case)
 global_map_points_3d = np.hstack([global_map_points, np.zeros((global_map_points.shape[0], 1))])
